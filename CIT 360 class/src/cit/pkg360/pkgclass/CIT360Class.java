687329c6fed846bda8135733c40a7cc6c3444b7a
Ronald Silva <sil17021@byui.edu>
2019-01-18 23:44:48 -0700

    01.18.17 first git

---
+++ CIT 360 class/src/cit/pkg360/pkgclass/CIT360Class.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cit.pkg360.pkgclass;
+
+/**
+ *
+ * @author ronsilva
+ */
+public class CIT360Class {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //new class hi Ron
+    }
+    
+}
