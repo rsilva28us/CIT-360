b9a71a87ad7891f4975e5ec5e3f6d77573ff4378
Ronald Silva <sil17021@byui.edu>
2019-01-19 13:33:12 -0700

    01.19.10 List collection

--- CIT 360 class/src/collections/ListCollection.java
+++ CIT 360 class/src/collections/ListCollection.java
@@ -1,12 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The List collection
+ * Objective of this class is to show an exemple of list collection
+ * Author: Ronald Silva
+ * Date last modified: Jan 19 2019
  */
 package collections;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
@@ -15,6 +15,32 @@ import java.util.List;
 public class ListCollection {
     
     public static void main(String[] args) {
-        List mylist = new ArrayList();
+        //List of Marvel movies
+        ArrayList<String> marvelMovies =new ArrayList<String>();
+            marvelMovies.add("Iron Man");
+            marvelMovies.add("The Incredible Hulk");
+            marvelMovies.add("Iron Man 2");
+            marvelMovies.add("Thor");
+            marvelMovies.add("Captain America: The First Avenger");
+            marvelMovies.add("Marvel's The Avengers");
+            marvelMovies.add("Iron Man 3");
+            marvelMovies.add("Thor: The Dark World");
+            marvelMovies.add("Captain America: The Winter Soldier");
+            marvelMovies.add("Guardians of the Galaxy");
+            marvelMovies.add("Avengers: Age of Ultron");
+            marvelMovies.add("Ant-Man");
+            marvelMovies.add("Captain America: Civil War");
+            marvelMovies.add("Doctor Strange");
+            marvelMovies.add("Guardians of the Galaxy Vol. 2");
+            marvelMovies.add("Spider-Man: Homecoming");
+            marvelMovies.add("Thor: Ragnarok");
+            marvelMovies.add("Black Panther");
+            marvelMovies.add("Avengers: Infinity War");
+            marvelMovies.add("Ant-Man and the Wasp");
+            
+                System.out.println("My Marvel movies list:"+marvelMovies);
+            
+       
+            
     }
 }
