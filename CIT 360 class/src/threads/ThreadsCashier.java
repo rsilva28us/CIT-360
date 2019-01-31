9189d7b0a746d9435d75bad709696c5b34903eb1
Ronald Silva <sil17021@byui.edu>
2019-01-30 22:22:10 -0700

    01.30.10 Threads update

--- CIT 360 class/src/threads/ThreadsCashier.java
+++ CIT 360 class/src/threads/ThreadsCashier.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The ThreadsCashier
+ * Objective of this class is to show an exemple of threads
+ * Author: Ronald Silva
+ * Date last modified: Jan 29 2019
  */
 package threads;
 
